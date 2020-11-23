5d0113ce5bc9cf0edc1f2b67c92a801f25b3215d
Wellington Migliari <49842956+wemigliari@users.noreply.github.com>
2020-11-23 15:49:35 +0100

    Add files via upload

--- bubblecoloredmap.R
+++ bubblecoloredmap.R
@@ -2,6 +2,49 @@ library(writexl)
 library(rgdal)
 library(dplyr)
 
+##### qtm (uninteractive map)
+
+### Shapefiles
+brazil <- read_sf("/Users/wemigliari/Documents/R/data/BRA_adm3.shp") ### Shape in form of sf
+saopaulo <- brazil%>%filter(NAME_2=="São Paulo")
+
+### Data on IDH
+idh0 <- read_excel("/Users/wemigliari/Documents/R/data/saopaulo_idh_2000_2010.xls", sheet = "idh")
+idh0 <- as.data.frame(idh0,na.rm = TRUE)
+names(idh0)[233] <- "NAME_3"
+
+### Merging shapefiles and data
+idh0 <- merge(saopaulo, idh0, by = "NAME_3")
+
+sp00 <- st_as_sf(idh0) ## It works!
+class(sp00)
+
+
+### Média
+
+qtm(sp00, fill = "RENOCUP", fill.breaks=c(0, 650, 1500, 2000, 3000, 4500, 12300, 16200),
+    fill.title = "Renda Média Ocupados 2000-2010",
+    fill.palette = "Purples", ## Blues, Reds, Purples etc for different colors. To invert the scale of the colors, use the minus sign, for instance, -Reds.
+    title = "Renda Média Ocupados 2000-2010",
+    title.position = c("left", "top")) +
+  tm_layout(asp=0) + 
+  tm_text("NAME_3", 
+          size = 0.6, 
+          remove.overlap = TRUE, 
+          auto.placement=FALSE) +
+  tm_compass (north = 0, type = "arrow", show.labels = 2) +
+  tm_layout(legend.title.size = .65, 
+            legend.text.size = .65, 
+            legend.frame = FALSE, title.size = 0.7,
+            outer.margins = c(0,0,0,0)) +
+  tm_credits("R Studio by Migliari, W. (2020).", position = c("left", "BOTTOM"))
+
+
+#################################################################################
+### Leaflet (interactive map) BUBBLES
+#################################################################################
+
+
 ### Shapefiles
 brazil1 <- read_sf("/Users/wemigliari/Documents/R/data/BRA_adm3.shp") ### Shape in form of sf
 saopaulo1 <- brazil1%>%filter(NAME_2=="São Paulo")
@@ -40,27 +83,7 @@ mytext <- paste(
   sep="") %>%
   lapply(htmltools::HTML)
 
-####
-
-
-qtm(sp1, fill = "RENOCUP", fill.breaks=c(0, 650, 1500, 2000, 3000, 4500, 12300, 16200),
-    fill.title = "Renda Média Ocupados 2000-2010",
-    fill.palette = "Reds", ## Blues, Reds, Purples etc for different colors. To invert the scale of the colors, use the minus sign, for instance, -Reds.
-    title = "Renda Média Ocupados 2000-2010",
-    title.position = c("center", "top")) +
-  tm_layout(asp=0) + 
-  tm_text("NAME_3", 
-          size = 0.6, 
-          remove.overlap = TRUE, 
-          auto.placement=FALSE) +
-  tm_compass (north = 0, type = "arrow", show.labels = 2) +
-  tm_layout(legend.title.size = .65, 
-            legend.text.size = .65, 
-            legend.frame = FALSE, title.size = 0.7,
-            outer.margins = c(0,0,0,0)) +
-  tm_credits("R Studio by Migliari, W. (2020).", position = c("right", "BOTTOM"))
-
-
+###
 
 library(RColorBrewer)
 
